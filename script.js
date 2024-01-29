db3a4c72ca4902f2313af8e04879d7a8cc10a064
SaifeddineBoudokhane <113269052+SaifeddineBoudokhane@users.noreply.github.com>
2024-01-29 15:18:23 +0100

    Make the function that plays a round

--- script.js
+++ script.js
@@ -20,14 +20,72 @@ function getComputerChoice(){
     let choice = generateRandom(0,3);
     switch(choice){
         case 0:
-            console.log("Rock")
+            return "rock"
             break;
         case 1:
-            console.log("Paper")
+            return "paper"
             break;
         case 2:
-            console.log("Scissors")
+            return "scissors"
             break;
     }
 }
-getComputerChoice()
+function getPlayerChoice(){
+    let player;
+    do {
+        player= prompt("Rock, Paper or Scissors ?","rock");
+        player=player.toLowerCase();
+    } while (player!="rock"&&player!="paper"&&player!="scissors");
+    return player
+}
+
+let computer=getComputerChoice();
+let player= getPlayerChoice();
+
+function round(computerSelection, playerSelection){
+    console.log(`You played ${playerSelection}`)
+    console.log(`Computer played ${computerSelection}`)
+    switch(playerSelection){
+        case "rock":
+            switch(computerSelection){
+                case "rock":
+                    console.log(`It's a Tie!`)
+                    break;
+                case "paper":
+                    console.log(`${computerSelection} beats ${playerSelection}, You Lose!`)
+                    break;
+                case "scissors":
+                    console.log(`${playerSelection} beats ${computerSelection}, You Win!`)
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerSelection){
+                case "rock":
+                    console.log(`${playerSelection} beats ${computerSelection}, You Win!`)
+                    break;
+                case "paper":
+                    console.log(`It's a Tie!`)
+                    break;
+                case "scissors":
+                    console.log(`${computerSelection} beats ${playerSelection}, You Lose!`)
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(computerSelection){
+                case "rock":
+                    console.log(`${computerSelection} beats ${playerSelection}, You Lose!`)
+                    break;
+                case "paper":
+                        console.log(`${playerSelection} beats ${computerSelection}, You Win!`)
+                    break;
+                case "scissors":
+                    console.log(`It's a Tie!`)
+                    break;
+            }
+            break;
+    }
+}
+
+round(computer,player);
